03d1ed6ebefa8af9dada2f1d07d405e1a8c15365
prosenrichersystems <philip.rosen@richersystems.ca>
2025-11-06 11:46:08 -0500

    Removed duplicate card on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -132,26 +132,6 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="bg-white overflow-hidden shadow-lg rounded-lg">
-                    <div class="p-6">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0">
-                                <div class="w-8 h-8 bg-purple-500 rounded-full flex items-center justify-center">
-                                    <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
-                                    </svg>
-                                </div>
-                            </div>
-                            <div class="ml-5 w-0 flex-1">
-                                <dl>
-                                    <dt class="text-sm font-medium text-gray-500 truncate">Used</dt>
-                                    <dd class="text-lg font-medium text-gray-900">{{ number_format($budgetStats['percentage_used'], 1) }}%</dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
 
             <!-- Progress Bar -->
